33ceb7bb9ec29973c8e83cbcf4569ed75a61bcc2
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2023-01-15 21:09:45 +0530

    quicksort

--- Arrays/sorting&searching/quicksort.cpp
+++ Arrays/sorting&searching/quicksort.cpp
@@ -1,34 +1,50 @@
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-int partition (int arr[], int low, int high){
-       // Your code here
-       int pivot = arr[low];
-       int i = low;
-       int j = high;
-       while(i<j){
-           while(arr[i]<=pivot && i<=high){
-               i++;
-           }
-           while(arr[j]>pivot && j>=low){
-               j--;
-           }
-           if(i<j){
-               swap(arr[i],arr[j]);
-           }
-
-       }
-       swap(arr[j],arr[low]);
-       return j;
+int partition(int arr[], int low, int high)
+{
+    // Your code here
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while (i < j)
+    {
+        while (arr[i] <= pivot && i <= high)
+        {
+            i++;
+        }
+        while (arr[j] > pivot && j >= low)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[j], arr[low]);
+    return j;
 }
 
-void quickSort(int arr[], int low, int high){
-        // code here
-        if(low<high){
-            int pivot = partition(arr,low,high);
-            quickSort(arr,low,pivot-1);
-            quickSort(arr,pivot+1,high);
-        }
+void quickSort(int arr[], int low, int high)
+{
+    // code here
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {4, 1, 3, 9, 7};
+    quickSort(arr, 0, 4);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
