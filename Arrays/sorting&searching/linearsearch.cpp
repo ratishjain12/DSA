17f1635293769a4605cb5f2763d5acdfe38a3f02
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2023-01-15 16:13:13 +0530

    linearSearch

---
+++ Arrays/sorting&searching/linearsearch.cpp
@@ -0,0 +1,13 @@
+
+
+int linearSearch(int arr[], int n, int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
