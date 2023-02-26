0f1d037fb21415a83d6d571d1f0d0c63bbedd440
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2023-02-26 13:55:47 +0530

    1D Dp

--- Dynammic Programming/1D DP/coin-change.cpp
+++ Dynammic Programming/1D DP/coin-change.cpp
@@ -1,61 +1,34 @@
-// top-down approach - recursive + memoization
-int solveMem(int coins[], int M, int target, vector<int> &dp)
+// Find maximum number of ways you can make the sum by using the denominations
+long long int solveTab(int coins[], int N, int sum)
 {
-    if (target == 0)
-        return 0;
-    if (target < 0)
-        return INT_MAX;
-    if (dp[target] != -1)
-        return dp[target];
-    int mini = INT_MAX;
-
-    for (int i = 0; i < M; i++)
+    long long int dp[N + 1][sum + 1];
+    for (int i = 0; i <= N; i++)
     {
-        int ans = solveMem(coins, M, target - coins[i], dp);
-        if (ans != INT_MAX)
-        {
-            mini = min(mini, 1 + ans);
-        }
+        dp[i][0] = 1; // if the sum value is zero maximum possible is 1
     }
-    dp[target] = mini;
-    return mini;
-}
-// M = size of array V = value for which you have to find number of coins
-int minCoins(int coins[], int M, int V)
-{
-    // Your code goes here
-    vector<int> dp(V + 1, -1);
-    int ans = solveMem(coins, M, V, dp);
-    if (ans == INT_MAX)
-        return -1;
-    else
-        return ans;
-}
-
-//--------------------------------------------------//
-
-// bottom-up approach - tabulation
-int solveTab(int coins[], int M, int V)
-{
-    vector<int> dp(V + 1, INT_MAX);
-    dp[0] = 0;
-    for (int i = 1; i <= V; i++)
+    for (int j = 0; j <= sum; j++)
     {
-        for (int j = 0; j < M; j++)
+        dp[0][j] = 0; // if there are zero coins and there is 0 ways to make the sum
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= sum; j++)
         {
-            if (i - coins[j] >= 0 && dp[i - coins[j]] != INT_MAX)
+            if (coins[i - 1] <= j)
             {
-                dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+                dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
             }
         }
     }
-    if (dp[V] == INT_MAX)
-        return -1;
-    return dp[V];
+    return dp[N][sum];
 }
-int minCoins(int coins[], int M, int V)
+long long int count(int coins[], int N, int sum)
 {
-    // Your code goes here
-    int ans = solveTab(coins, M, V);
-    return ans;
+
+    // code here.
+    return solveTab(coins, N, sum);
 }
---
+++ Dynammic Programming/1D DP/combination-sum-IV.cpp
@@ -0,0 +1,45 @@
+// number of possible combinations that add upto the target
+
+// top-downa-approach
+
+int solveRec(vector<int> &nums, int target, vector<int> &dp)
+{
+    if (target < 0)
+        return 0;
+    if (target == 0)
+        return 1;
+    if (dp[target] != -1)
+        return dp[target];
+    int ans = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        ans += solveRec(nums, target - nums[i], dp);
+    }
+    dp[target] = ans;
+    return dp[target];
+}
+int combinationSum4(vector<int> &nums, int target)
+{
+    vector<int> dp(target + 1, -1);
+    return solveRec(nums, target, dp);
+}
+
+// bottom-up-approach
+
+int combinationSum4(vector<int> &nums, int target)
+{
+    vector<long int> dp(target + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= target; i++)
+    {
+        for (int j = 0; j < nums.size(); j++)
+        {
+            if (i - nums[j] >= 0)
+            {
+                dp[i] += dp[i - nums[j]] % mod;
+            }
+        }
+    }
+
+    return dp[target];
+}
