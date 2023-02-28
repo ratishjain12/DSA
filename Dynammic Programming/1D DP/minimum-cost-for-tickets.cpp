0cac09e1f97c22cec08ab5331039d919d5a0bf2d
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2023-02-28 23:19:25 +0530

    2D DP

--- Dynammic Programming/1D DP/minimum-cost-for-tickets.cpp
+++ Dynammic Programming/1D DP/minimum-cost-for-tickets.cpp
@@ -38,12 +38,10 @@ int mincostTickets(vector<int> &days, vector<int> &costs)
         int option1 = costs[0] + dp[k + 1];
 
         int i;
-        for (i = k; i < n && days[i] < days[k] + 7; i++)
-            ;
+        for (i = k; i < n && days[i] < days[k] + 7; i++);
         int option2 = costs[1] + dp[i];
 
-        for (i = k; i < n && days[i] < days[k] + 30; i++)
-            ;
+        for (i = k; i < n && days[i] < days[k] + 30; i++);
         int option3 = costs[2] + dp[i];
 
         dp[k] = min(option1, min(option2, option3));
---
+++ Dynammic Programming/2D DP/largest-area-square-matrix.cpp
@@ -0,0 +1,89 @@
+// recursive
+int solve(vector<vector<char>> &matrix, int i, int j, int &maxi)
+{
+    if (i >= matrix.size() || j >= matrix[0].size())
+        return 0;
+    int right = solve(matrix, i, j + 1, maxi);
+    int diagonal = solve(matrix, i + 1, j + 1, maxi);
+    int down = solve(matrix, i + 1, j, maxi);
+    if (matrix[i][j] == '1')
+    {
+        int ans = 1 + min(right, min(diagonal, down));
+        maxi = max(maxi, ans);
+        return ans;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int maximalSquare(vector<vector<char>> &matrix)
+{
+
+    int maxi = 0;
+    solve(matrix, 0, 0, maxi);
+    return maxi * maxi;
+}
+
+// Recursive + Memoization(Top-down)
+int solve(vector<vector<char>> &matrix, int i, int j, int &maxi, vector<vector<int>> &dp)
+{
+    if (i >= matrix.size() || j >= matrix[0].size())
+        return 0;
+    if (dp[i][j] != -1)
+        return dp[i][j];
+    int right = solve(matrix, i, j + 1, maxi, dp);
+    int diagonal = solve(matrix, i + 1, j + 1, maxi, dp);
+    int down = solve(matrix, i + 1, j, maxi, dp);
+    if (matrix[i][j] == '1')
+    {
+        dp[i][j] = 1 + min(right, min(diagonal, down));
+        maxi = max(maxi, dp[i][j]);
+        return dp[i][j];
+    }
+    else
+    {
+        return 0;
+    }
+}
+int maximalSquare(vector<vector<char>> &matrix)
+{
+    int n = matrix.size();
+    int m = matrix[0].size();
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+    int maxi = 0;
+    solve(matrix, 0, 0, maxi, dp);
+    return maxi * maxi;
+}
+
+// Tabulation (Bottom-up Approach)
+int solve(vector<vector<char>> &matrix)
+{
+    int maxi = 0;
+    int row = matrix.size();
+    int col = matrix[0].size();
+    vector<vector<int>> dp(row + 1, vector<int>(col + 1, 0));
+    for (int i = row - 1; i >= 0; i--)
+    {
+        for (int j = col - 1; j >= 0; j--)
+        {
+            int right = dp[i][j + 1];
+            int diagonal = dp[i + 1][j + 1];
+            int down = dp[i + 1][j];
+            if (matrix[i][j] == '1')
+            {
+                dp[i][j] = 1 + min(right, min(diagonal, down));
+                maxi = max(maxi, dp[i][j]);
+            }
+            else
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+    return maxi * maxi;
+}
+int maximalSquare(vector<vector<char>> &matrix)
+{
+    return solve(matrix);
+}
