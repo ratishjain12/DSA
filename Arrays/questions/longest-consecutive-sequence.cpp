dbf15fb43fd6917b4526d048a16c501850ecf7ba
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2023-04-05 21:15:39 +0530

    fix

--- Arrays/questions/longest-consecutive-sequence.cpp
+++ Arrays/questions/longest-consecutive-sequence.cpp
@@ -1,28 +1,30 @@
-int n = nums.size();
-int longest = 1;
-unordered_set<int> st;
-if (nums.size() == 0)
+int longestSequence(vector<int> &nums)
 {
-    return 0;
-}
-for (int i = 0; i < nums.size(); i++)
-{
-    st.insert(nums[i]);
-}
-
-for (auto it : st)
-{
-    if (st.find(it - 1) == st.end())
+    int n = nums.size();
+    int longest = 1;
+    unordered_set<int> st;
+    if (nums.size() == 0)
     {
-        int cnt = 1;
-        int x = it;
-        while (st.find(x + 1) != st.end())
-        {
-            x = x + 1;
-            cnt += 1;
-        }
-        longest = max(longest, cnt);
+        return 0;
     }
-}
-return longest;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        st.insert(nums[i]);
+    }
+
+    for (auto it : st)
+    {
+        if (st.find(it - 1) == st.end())
+        {
+            int cnt = 1;
+            int x = it;
+            while (st.find(x + 1) != st.end())
+            {
+                x = x + 1;
+                cnt += 1;
+            }
+            longest = max(longest, cnt);
+        }
+    }
+    return longest;
 }
