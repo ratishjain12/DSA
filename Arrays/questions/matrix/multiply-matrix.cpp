1b48e29b40f3ec9b31b787fb72fcb47984add7e7
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2023-04-15 10:56:11 +0530

    matrix questions

--- Arrays/questions/matrix/multiply-matrix.cpp
+++ Arrays/questions/matrix/multiply-matrix.cpp
@@ -1,14 +1,13 @@
 vector<vector<int>> multiplyMatrix(const vector<vector<int>> &a, const vector<vector<int>> &b)
 {
     // code here
-
     int n1 = a.size();
     int m1 = a[0].size();
     int n2 = b.size();
     int m2 = b[0].size();
     vector<vector<int>> ans(n1, vector<int>(m2, 0));
 
-    if (a[0].size() != b.size())
+    if (a[0].size() != b.size()) // check if columns in first matrix  == rows in second matrix
         return {{-1}};
     for (int i = 0; i < n1; i++)
     {
