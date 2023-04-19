2bbccc20c35b26277b920ace5d3d983b3b0735ab
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2023-04-19 22:31:31 +0530

    tree questions

---
+++ Trees/binary-trees/medium/check-if-subtree.cpp
@@ -0,0 +1,30 @@
+bool isSame(Node *T, Node *S)
+{
+    if (T == NULL && S == NULL)
+    {
+        return true;
+    }
+    if (T == NULL || S == NULL)
+    {
+        return false;
+    }
+    if (T->data != S->data)
+    {
+        return false;
+    }
+    return isSame(T->left, S->left) && isSame(T->right, S->right);
+}
+bool isSubTree(Node *T, Node *S)
+{
+    // Your code here
+    if (T == NULL && S == NULL)
+    {
+        return true;
+    }
+    if (T == NULL || S == NULL)
+    {
+        return false;
+    }
+
+    return isSame(T, S) || isSubTree(T->left, S) || isSubTree(T->right, S);
+}
