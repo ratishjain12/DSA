cc520754412422443059dac6522d953d66f721a2
ratishjain <ratishjain6@gmail.com>
2023-05-06 14:24:59 +0530

    vertical order traversal

---
+++ Trees/binary-trees/medium/vertical-order.cpp
@@ -0,0 +1,31 @@
+    /*
+    If there are multiple nodes passing through a vertical line, then they should be printed as they 
+    appear in level order traversal of the tree.
+    */
+    vector<int> verticalOrder(Node *root){
+        //Your code here
+        queue<pair<Node*,int>>q;
+        map<int,vector<int>>mp;
+        q.push({root,0});
+        while(!q.empty()){
+            auto p = q.front();
+            Node *front = p.first;
+            int line = p.second;
+            mp[line].push_back(front->data);
+            q.pop();
+            if(front->left){
+                q.push({front->left,line-1});
+            }
+            if(front->right){
+                q.push({front->right,line+1});
+            }
+            
+        }
+        vector<int>ans;
+        for(auto i:mp){
+            for (auto j:i.second){
+                ans.push_back(j);
+            }
+        }
+        return ans;
+    }
