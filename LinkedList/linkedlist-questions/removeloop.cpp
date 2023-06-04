35291157410295a2b9c48b7d2af40120a52c55f4
ratishjain <ratishjain6@gmail.com>
2023-06-04 18:02:30 +0530

    remove loop

--- LinkedList/linkedlist-questions/removeloop.cpp
+++ LinkedList/linkedlist-questions/removeloop.cpp
@@ -1,25 +1,31 @@
-void removeLoop(Node* head)
-{
-    // code here
-    // just remove the loop without losing any nodes
-    Node *slow = head,*fast = head;
-    while(fast&&fast->next){
-        slow = slow->next;
-        fast = fast->next->next;
-        if(slow == fast) break;
-    }
-    if(slow!=fast) return;
-    slow = head;
-    if(slow!=fast){
-        while(slow->next!=fast->next){
-         slow = slow->next;
-         fast = fast->next;
+    void removeLoop(Node* head)
+    {
+        // code here
+        // just remove the loop without losing any nodes
+        if(head == NULL || head->next == NULL){
+            return;
         }
-    }else{
-        while(fast->next!=slow){
-            fast = fast->next;
+        Node *slow = head;
+        Node *fast = head;
+        Node *entry = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) break;
         }
+        
+        if(slow == head){ // if slow and fast meet at head then move only fast 
+            while(fast->next != slow){
+                fast = fast->next;
+            }
+            fast->next = NULL;
+        }else if(slow == fast){
+            slow = head;
+            while(slow->next != fast->next){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            fast->next = NULL;
+        }
+        
     }
-
-    fast->next = NULL;
-}
