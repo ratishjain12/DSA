5f3d6d76c435dc53fe20caef62eab6a485696ec7
ratishjain <ratishjain6@gmail.com>
2023-06-09 21:37:41 +0530

    clone ll and add two numbers ll

--- LinkedList/linkedlist-questions/add-two-numbers.cpp
+++ LinkedList/linkedlist-questions/add-two-numbers.cpp
@@ -72,3 +72,67 @@ struct Node* addTwoLists(struct Node* first, struct Node* second){
     ansHead = reverse(ansHead);// reverse ans list
     return ansHead;
 }
+
+
+// similar code but shorter
+Node *reverse(Node *head){
+    Node *curr = head;
+    Node *prev = NULL;
+    while(curr){
+        Node *temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    
+    return prev;
+}
+
+void insert(Node *&head,Node *&tail, int data){
+    Node *temp = new Node(data);
+    
+    if(head == NULL){
+        head = tail = temp;
+    }else{
+        tail->next = temp;
+        tail = tail->next;
+    }
+}
+Node *add(Node *first,Node *second){
+    int carry = 0;
+    Node *ansHead = NULL;
+    Node *ansTail = NULL;
+    while(first != NULL || second != NULL || carry != 0){
+        int val1 = 0;
+        int val2 = 0;
+        if(first != NULL){
+            val1 = first->data;
+        }
+        if(second != NULL){
+            val2 = second->data;
+        }
+        int sum = carry + val1 + val2;
+        int digit = sum%10;
+        insert(ansHead,ansTail,digit);
+        carry = sum/10;
+        if(first){
+            first = first->next;
+        }
+        if(second){
+            second = second->next;
+        }
+    }
+    
+    return ansHead;
+}
+
+
+struct Node* addTwoLists(struct Node* first, struct Node* second){
+    // code here
+    Node *firstList = reverse(first); // reverse first list
+    Node *secondList = reverse(second);// reverse second list
+    
+    Node *ansHead = add(firstList,secondList);
+    ansHead = reverse(ansHead);// reverse ans list
+    return ansHead;
+}
