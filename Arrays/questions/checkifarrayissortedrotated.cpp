bf0c2ae9b61c79df9d4746e79b31777369a8de4d
ratishjain <ratishjain6@gmail.com>
2023-07-13 20:37:39 +0530

    check if array sorted and rotated gfg version

--- Arrays/questions/checkifarrayissortedrotated.cpp
+++ Arrays/questions/checkifarrayissortedrotated.cpp
@@ -1,3 +1,4 @@
+// this if array is sorted in non decreasing order and then rotated
 bool check(vector<int> &nums)
 {
     int cnt = 0;
@@ -27,4 +28,46 @@ bool check(vector<int> &nums)
         }
     }
     return cnt <= 1;
+}
+
+//gfg version where both non decreasing and non increasing will be given
+
+bool isIncreasing(int arr[],int num){
+    int cnt = 0;
+    if(arr[0] < arr[num-1]) return false;
+    for(int i = 1;i<num;i++){
+        if(arr[i-1] > arr[i]){
+            cnt++;
+        }
+    }
+    
+    if(arr[num-1] > arr[0]){
+        cnt++;
+    }
+    return cnt<=1;
+}
+
+
+
+bool isDecreasing(int arr[],int num){
+    int cnt = 0;
+    if(arr[0] > arr[num-1]) return false;
+    for(int i = 1;i<num;i++){
+        if(arr[i-1] < arr[i]){
+            cnt++;
+        }
+    }
+    
+    if(arr[num-1] < arr[0]){
+        cnt++;
+    }
+    
+    return cnt<=1;
+}
+bool checkRotatedAndSorted(int arr[], int num){
+    
+    // Your code here
+    if(isIncreasing(arr,num)) return true;
+    if(isDecreasing(arr,num)) return true;
+    return false;
 }
