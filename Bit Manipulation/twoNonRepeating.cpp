541f98c34fb18c945e0855c753e4049a16316424
ratishjain <ratishjain6@gmail.com>
2024-03-23 00:47:12 +0530

    two non repeating elements in an array

--- Bit Manipulation/twoNonRepeating.cpp
+++ Bit Manipulation/twoNonRepeating.cpp
@@ -6,5 +6,18 @@ vector<int>twoNonRepeating(vector<int>num){
         xxory^=i;
     }
 
-    int rightMostBitMask;
+    int rightMostBitMask = ;
+
+    int x = 0;
+    int y = 0;
+    for(int i : num){
+        if(i & rightMostBitMask){
+            x^=i;
+        }else{
+            y^=i;
+        }
+    }
+
+    if(x<y) return {x,y};
+    else return {y,x};
 }
